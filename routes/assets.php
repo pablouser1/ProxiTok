493f56a0523f681a37d059cf329bd6f90006c230
Pablo Ferreiro <pferreiromero@gmail.com>
2022-01-05 21:25:49 +0100

    More details of user or tag

--- routes/assets.php
+++ routes/assets.php
@@ -15,41 +15,6 @@ function isValidDomain(string $url): bool {
     return count($host_split) === 3 && in_array($host_split[1] . '.' . $host_split[2], $valid_domains);
 }
 
-Route::add('/images', function () {
-    if (!isset($_GET['url'])) {
-		die('You need to send a url!');
-	}
-    $url = $_GET['url'];
-    if (!filter_var($url, FILTER_VALIDATE_URL) || !isValidDomain($url)) {
-        die('Not a valid URL');
-    }
-    $img = file_get_contents($url, false, stream_context_create(['http' => ['ignore_errors' => true]]));
-    if ($img) {
-        header('Content-Type: image/jpeg');
-        return $img;
-    } else {
-        return 'Error while getting image!';
-    }
-});
-
-Route::add('/audios', function () {
-    if (!isset($_GET['url'])) {
-		die('You need to send a url!');
-	}
-    $url = $_GET['url'];
-
-    if (!filter_var($url, FILTER_VALIDATE_URL) || !isValidDomain($url)) {
-        die('Not a valid URL');
-    }
-    $audio = file_get_contents($url, false, stream_context_create(['http' => ['ignore_errors' => true]]));
-    if ($audio) {
-        header('Content-Type: audio/mp3');
-        return $audio;
-    } else {
-        return 'Error while getting audio!';
-    }
-});
-
 Route::add('/stream', function () {
 	if (!isset($_GET['url'])) {
 		die('You need to send a url!');
@@ -61,9 +26,9 @@ Route::add('/stream', function () {
     }
 
     if (isset($_GET['download'])) {
-        // Download
+        // Download (video only)
         $downloader = new \Sovit\TikTok\Download();
-        $downloader->url($url,"tiktok-video",'mp4');
+        $downloader->url($url, "tiktok-video", 'mp4');
     } else {
         // Stream
         $streamer = new \Sovit\TikTok\Stream();
--- routes/index.php
+++ routes/index.php
@@ -29,6 +29,20 @@ function getLatte(): \Latte\Engine {
         $path = "{$subdir}/{$name}";
         return $path;
     });
+    // https://stackoverflow.com/a/36365553
+    $latte->addFunction('number', function (int $x) {
+        if($x > 1000) {
+            $x_number_format = number_format($x);
+            $x_array = explode(',', $x_number_format);
+            $x_parts = array('K', 'M', 'B', 'T');
+            $x_count_parts = count($x_array) - 1;
+            $x_display = $x;
+            $x_display = $x_array[0] . ((int) $x_array[1][0] !== 0 ? '.' . $x_array[1][0] : '');
+            $x_display .= $x_parts[$x_count_parts - 1];
+            return $x_display;
+        }
+        return $x;
+    });
     return $latte;
 }
 
