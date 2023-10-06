a3b4143ab198175c29db10bd682296fe24643442
Pablo Ferreiro <me@pabloferreiro.es>
2023-10-07 00:17:11 +0200

    Show debug info when TikTok gives error

--- app/Helpers/ErrorHandler.php
+++ app/Helpers/ErrorHandler.php
@@ -7,7 +7,7 @@ use TikScraper\Models\Meta;
 class ErrorHandler {
     public static function showMeta(Meta $meta) {
         http_response_code($meta->httpCode);
-        Wrappers::latte('error', new ErrorTemplate($meta->httpCode, $meta->proxitokMsg, $meta->proxitokCode));
+        Wrappers::latte('error', new ErrorTemplate($meta->httpCode, $meta->proxitokMsg, $meta->proxitokCode, $meta->response));
     }
 
     public static function showText(int $code, string $msg) {
--- app/Models/ErrorTemplate.php
+++ app/Models/ErrorTemplate.php
@@ -1,16 +1,20 @@
 <?php
 namespace App\Models;
+use TikScraper\Models\Response;
 
 class ErrorTemplate extends BaseTemplate {
     public int $http_code = 502;
     public ?int $tiktok_code = -1;
     public string $msg = '';
 
+    public ?Response $response = null;
 
-    function __construct(int $http_code, string $msg, ?int $tiktok_code = null) {
+
+    function __construct(int $http_code, string $msg, ?int $tiktok_code = null, ?Response $response) {
         parent::__construct('Error');
         $this->http_code = $http_code;
         $this->msg = $msg;
         $this->tiktok_code = $tiktok_code;
+        $this->response = $response;
     }
 }
